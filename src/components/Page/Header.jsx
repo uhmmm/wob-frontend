5cd2c1bd405daba30080cb6d0341a894c36dcf39
Zaturrby <rjkorteschiel@gmail.com>
2019-09-17 16:28:17 +0200

    trigger deploy

--- src/components/Page/Header.jsx
+++ src/components/Page/Header.jsx
@@ -7,11 +7,14 @@ import { type } from '../../styles'
 const HeaderContainer = styled.main({})
 
 const Title = styled.h1({ ...type.title.large })
+const StyledLink = styled(Link)({
+  ...type.content.large
+})
 
 const Header = () => (
   <HeaderContainer>
     <Title>Header</Title>
-    <Link to="/form/start">Start je Wob-verzoek</Link>
+    <StyledLink to="/form/start">Start je Wob-verzoek</StyledLink>
   </HeaderContainer>
 )
 
