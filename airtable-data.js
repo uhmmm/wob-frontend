6af19a38748821540479541284cdaf6a51d617dc
Zaturrby <rjkorteschiel@gmail.com>
2019-09-20 17:46:27 +0200

    get data from airtable

--- airtable-data.js
+++ airtable-data.js
@@ -1,37 +1,32 @@
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('appe4s599Wi0qoxtJ');
+var Airtable = require('airtable')
+var fs = require('fs')
 
-base('Table 1').select({
-    // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
-    view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
+var APIKEY = process.env.AIRTABLE_KEY
+var base = new Airtable({ apiKey: APIKEY }).base('appe4s599Wi0qoxtJ')
 
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('asideId'));
-    });
+let infoAsides = []
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
+base('infoAsides')
+  .select({
+    view: 'Table'
+  })
+  .eachPage(
+    function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+        infoAsides.push(record.fields)
+      })
 
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
-OUTPUT
-Retrieved 1
- Retrieved 2
- Retrieved 3
-FETCH FIRST PAGE
-// If you only want the first page of records, you can
-// use `firstPage` instead of `eachPage`.
-base('Table 1').select({
-    view: 'Grid view'
-}).firstPage(function(err, records) {
-    if (err) { console.error(err); return; }
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('asideId'));
-    });
-});
+      fetchNextPage()
+    },
+    function done(err) {
+      fs.writeFileSync(
+        './data.json',
+        JSON.stringify(infoAsides, null, 2),
+        'utf-8'
+      )
+      if (err) {
+        console.error(err)
+        return
+      }
+    }
+  )
