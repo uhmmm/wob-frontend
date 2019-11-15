a27fbd06880f6683c7e23907b56cce122e30d609
Zaturrby <rjkorteschiel@gmail.com>
2019-11-15 12:05:20 +0100

    replace images

--- src/components/ReviewBoxList.jsx
+++ src/components/ReviewBoxList.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
+import React from "react";
+import styled from "@emotion/styled";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
-import { getRouteBySlug } from '../reducers/routes'
-import { getElementsByProperty } from '../reducers/formElements'
-import { colors } from '../styles'
-import { FormElementResolverConnected } from './FormElementResolver'
+import { getRouteBySlug } from "../reducers/routes";
+import { getElementsByProperty } from "../reducers/formElements";
+import { colors } from "../styles";
+import { FormElementResolverConnected } from "./FormElementResolver";
 
 const ReviewBoxContainer = styled.div({
   background: colors.yellow,
-  margin: '0 0 4rem 0',
-  borderRadius: '10px',
-  padding: '2rem'
-})
+  margin: "0 0 4rem 0",
+  borderRadius: "10px",
+  padding: "2rem"
+});
 
 const ReviewBox = ({ rootElementId }) => {
   return (
     <ReviewBoxContainer>
       <FormElementResolverConnected formElId={rootElementId} />
     </ReviewBoxContainer>
-  )
-}
+  );
+};
 
 const ReviewBoxListContainer = styled.div({
-  position: 'absolute',
-  top: '22rem',
-  right: '2rem',
-  width: '20rem'
-})
+  position: "absolute",
+  top: "5rem",
+  right: "2rem",
+  width: "20rem"
+});
 
 const ReviewBoxList = ({ rootElementIds }) => {
   return rootElementIds.length > 0 ? (
@@ -36,26 +36,26 @@ const ReviewBoxList = ({ rootElementIds }) => {
       <ReviewBox rootElementId={rootElementIds[0]} />
       <ReviewBox rootElementId={rootElementIds[1]} wobKnop />
     </ReviewBoxListContainer>
-  ) : null
-}
+  ) : null;
+};
 
 const mapStateToProps = (state, { match }) => {
-  let route = getRouteBySlug(state, { slug: match.params.formSlug })
+  let route = getRouteBySlug(state, { slug: match.params.formSlug });
 
   let rootElementIds = getElementsByProperty(state, {
-    partOf: ['reviewBox/1', 'reviewBox/2'],
+    partOf: ["reviewBox/1", "reviewBox/2"],
     routeId: [route.routeId],
-    type: 'root'
+    type: "root"
   }).map(value => {
-    return value && value.elementId
-  })
+    return value && value.elementId;
+  });
 
   return {
     rootElementIds
-  }
-}
+  };
+};
 
 const ReviewBoxListConnected = withRouter(
   connect(mapStateToProps)(ReviewBoxList)
-)
-export { ReviewBoxListConnected }
+);
+export { ReviewBoxListConnected };
